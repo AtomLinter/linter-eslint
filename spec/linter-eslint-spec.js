196f184c9607166c51a5a93af734df2e0a03fa52
Landon Abney <Landonabney@gmail.com>
2015-11-20 00:10:10 -0800

    Attempt to get the specs working
    WIP, does not work yet.

--- spec/linter-eslint-spec.js
+++ spec/linter-eslint-spec.js
@@ -6,9 +6,14 @@ describe('The eslint provider for Linter', () => {
   const lint = require('../lib/main.js').provideLinter().lint;
 
   beforeEach(() => {
-    atom.workspace.destroyActivePaneItem();
     waitsForPromise(() => {
-      return atom.packages.activatePackage('linter-eslint');
+      atom.packages.activatePackage('linter-eslint');
+      /* We need a way to either force the worker process to spawn, or wait
+       *  on the timeout here, or all the tests fail.
+       */
+      return atom.packages.activatePackage('language-javascript').then(() =>
+        atom.workspace.open(__dirname + '/files/good.js')
+      );
     });
   });
 
@@ -31,49 +36,50 @@ describe('The eslint provider for Linter', () => {
     });
 
     it('finds at least one message', () => {
-      waitsForPromise(() => {
-        return lint(editor).then(messages => {
-          expect(messages.length).toEqual(1);
-        });
+      /* The following doesn't work, throwing a "Error: Pane has been destroyed"
+       *  in the console and not actually running the tests.
+       */
+      // return atom.workspace.open(__dirname + '/files/empty.js').then(editor => {
+      return lint(editor).then(messages => {
+        expect(messages.length).toEqual(1);
       });
     });
 
     it('verifies that message', () => {
-      waitsForPromise(() => {
-        return lint(editor).then(messages => {
-          expect(messages[0].type).toBeDefined();
-          expect(messages[0].type).toEqual('Error');
-          expect(messages[0].text).not.toBeDefined();
-          expect(messages[0].html).toBeDefined();
-          expect(messages[0].html).toEqual('<span class="badge badge-flexible">' +
-            'no-undef</span> &quot;foo&quot; is not defined.');
-          expect(messages[0].filePath).toBeDefined();
-          expect(messages[0].filePath).toMatch(/.+specs\/files\/bad\.js$/);
-          expect(messages[0].range).toBeDefined();
-          expect(messages[0].range.length).toEqual(2);
-          expect(messages[0].range).toEqual([[0, 0], [0, 8]]);
-        });
+      return lint(editor).then(messages => {
+        expect(messages[0].type).toBeDefined();
+        expect(messages[0].type).toEqual('Error');
+        expect(messages[0].text).not.toBeDefined();
+        expect(messages[0].html).toBeDefined();
+        expect(messages[0].html).toEqual('<span class="badge badge-flexible">' +
+          'no-undef</span> &quot;foo&quot; is not defined.');
+        expect(messages[0].filePath).toBeDefined();
+        expect(messages[0].filePath).toMatch(/.+spec[\\\/]files[\\\/]bad\.js$/);
+        expect(messages[0].range).toBeDefined();
+        expect(messages[0].range.length).toEqual(2);
+        expect(messages[0].range).toEqual([[0, 0], [0, 8]]);
       });
     });
   });
 
   it('finds nothing wrong with an empty file', () => {
-    waitsForPromise(() => {
-      return atom.workspace.open(__dirname + '/files/empty.js').then(editor => {
-        return lint(editor).then(messages => {
-          expect(messages.length).toEqual(0);
-        });
+    return atom.workspace.open(__dirname + '/files/empty.js').then(editor => {
+      return lint(editor).then(messages => {
+        expect(messages.length).toEqual(0);
       });
     });
   });
 
   it('finds nothing wrong with a valid file', () => {
-    waitsForPromise(() => {
-      return atom.workspace.open(__dirname + '/files/good.js').then(editor => {
-        return lint(editor).then(messages => {
-          expect(messages.length).toEqual(0);
-        });
+    return atom.workspace.open(__dirname + '/files/good.js').then(editor => {
+      return lint(editor).then(messages => {
+        expect(messages.length).toEqual(0);
       });
     });
   });
+
+  // TODO: Lol...
+  it('purposely fails', () => {
+    return expect(0).toBe(1);
+  })
 });
