f896a0de9828993d04eab1454db08df1ee97e054
Ian VanSchooten <ivanschooten@nutshell.com>
2015-11-28 22:37:14 -0500

    Add tests for determineConfigFile helper

---
+++ spec/es5-helpers-spec.js
@@ -0,0 +1,131 @@
+'use babel';
+
+import Path from 'path'
+import OS from 'os'
+import {mkdir, cp, rm} from 'shelljs'
+
+let fixtureDir
+
+describe('The es5 linter-eslint helper', () => {
+  const {determineConfigFile} = require('../lib/es5-helpers');
+
+  /**
+   * Returns the path inside of the fixture directory.
+   * @returns {string} The path inside the fixture directory.
+   * @private
+   */
+  function getFixturePath() {
+    const args = Array.prototype.slice.call(arguments);
+    args.unshift(fixtureDir);
+    return Path.join.apply(Path, args);
+  }
+
+  it('test setup', () => {
+    // copy into clean area so as not to get "infected" by this project's .eslintrc files
+    fixtureDir = OS.tmpdir() + '/linter-eslint/fixtures'
+    mkdir('-p', fixtureDir)
+    cp('-r', './spec/fixtures/.', fixtureDir)
+  })
+
+  describe('determineConfigFile', () => {
+    it('finds a .eslintrc file', () => {
+      const dir = getFixturePath('configs/no-ext/')
+      const expectedPath = Path.join(dir, '.eslintrc')
+      const params = { fileDir: dir }
+      const foundConfigFile = determineConfigFile(params)
+      expect(foundConfigFile).toEqual(expectedPath)
+    })
+
+    it('finds an .eslintrc.yaml file', () => {
+      const dir = getFixturePath('configs/yaml/')
+      const expectedPath = Path.join(dir, '.eslintrc.yaml')
+      const params = { fileDir: dir }
+      const foundConfigFile = determineConfigFile(params)
+      expect(foundConfigFile).toEqual(expectedPath)
+    })
+
+    it('finds an .eslintrc.yml file', () => {
+      const dir = getFixturePath('configs/yml/')
+      const expectedPath = Path.join(dir, '.eslintrc.yml')
+      const params = { fileDir: dir }
+      const foundConfigFile = determineConfigFile(params)
+      expect(foundConfigFile).toEqual(expectedPath)
+    })
+
+    it('finds an .eslintrc.js file', () => {
+      const dir = getFixturePath('configs/js/')
+      const expectedPath = Path.join(dir, '.eslintrc.js')
+      const params = { fileDir: dir }
+      const foundConfigFile = determineConfigFile(params)
+      expect(foundConfigFile).toEqual(expectedPath)
+    })
+
+    it('finds an .eslintrc.json file', () => {
+      const dir = getFixturePath('configs/json/')
+      const expectedPath = Path.join(dir, '.eslintrc.json')
+      const params = { fileDir: dir }
+      const foundConfigFile = determineConfigFile(params)
+      expect(foundConfigFile).toEqual(expectedPath)
+    })
+
+    it('finds a package.json file with an eslintConfig section', () => {
+      const dir = getFixturePath('configs/package-json/')
+      const expectedPath = Path.join(dir, 'package.json')
+      const params = { fileDir: dir }
+      const foundConfigFile = determineConfigFile(params)
+      expect(foundConfigFile).toEqual(expectedPath)
+    })
+
+    describe('if no configuration file is found', () => {
+      it('returns undefined if no configFile option is set', () => {
+        const dir = getFixturePath('configs/none/')
+        const expectedPath = undefined
+        const params = { fileDir: dir }
+        const foundConfigFile = determineConfigFile(params)
+        expect(foundConfigFile).toEqual(expectedPath)
+      })
+
+      it('returns null if canDisable option is set', () => {
+        const dir = getFixturePath('configs/none/')
+        const expectedPath = null
+        const params = {
+          fileDir: dir,
+          canDisable: true
+        }
+        const foundConfigFile = determineConfigFile(params)
+        expect(foundConfigFile).toEqual(expectedPath)
+      })
+
+      it('returns null if canDisable option is set, even if configFile option is set', () => {
+        const dir = getFixturePath('configs/none/')
+        const expectedPath = null
+        const params = {
+          fileDir: dir,
+          canDisable: true,
+          configFile: '/home/test/config.js'
+        }
+        const foundConfigFile = determineConfigFile(params)
+        expect(foundConfigFile).toEqual(expectedPath)
+      })
+
+      it('returns specified configFile if option is set and canDisable is false', () => {
+        const dir = getFixturePath('configs/none/')
+        const expectedPath = '/home/test/config.js'
+        const params = {
+          fileDir: dir,
+          canDisable: false,
+          configFile: expectedPath
+        }
+        const foundConfigFile = determineConfigFile(params)
+        expect(foundConfigFile).toEqual(expectedPath)
+      })
+    })
+
+
+  })
+
+  it('teardown', () => {
+    // Clean up temporary directory
+    rm('-r', fixtureDir);
+  })
+})
