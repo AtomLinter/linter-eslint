4b092faeacb4e2094fab22f62cc81d2ff51bafda
steelbrain <aneesiqbalbhatti@gmail.com>
2015-11-30 16:55:44 -0700

    :new: Add src folder with source js files

---
+++ src/helpers.js
@@ -0,0 +1,36 @@
+'use babel'
+
+import ChildProcess from 'child_process'
+import CP from 'childprocess-promise'
+
+export function spawnWorker() {
+  let shouldLive = true
+  const env = Object.create(process.env)
+  delete env.NODE_PATH
+  delete env.NODE_ENV
+  const data = {stdout: [], stderr: []}
+  const child = ChildProcess.fork(__dirname + '/worker.js', [], {env, silent: true})
+  const worker = new CP(child)
+  function killer() {
+    shouldLive = false
+    child.kill()
+  }
+  child.stdout.on('data', function(chunk) {
+    data.stdout.push(chunk)
+  })
+  child.stderr.on('data', function(chunk) {
+    data.stderr.push(chunk)
+  })
+  child.on('exit', function() {
+    if (shouldLive) {
+      console.log('ESLint Worker Info', {stdout: data.stdout.join(''), stderr: data.stderr.join('')})
+      atom.notifications.addWarning('[Linter-ESLint] Worker died unexpectedly', {detail: 'Check your console for more info. A new worker will be spawned instantly.', dismissable: true})
+    }
+    child.emit('exit-linter', shouldLive)
+  })
+  process.on('exit', killer)
+  return {child, worker, subscription: {dispose: function() {
+    killer()
+    process.removeListener('exit', killer)
+  }}}
+}
--- src/main.js
+++ src/main.js
@@ -1,25 +1,11 @@
-'use strict';
-'use babel';
+'use babel'
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
+import Path from 'path'
+import {CompositeDisposable} from 'atom'
+import {spawnWorker} from './helpers'
+import escapeHTML from 'escape-html'
 
-var _path = require('path');
-
-var _path2 = _interopRequireDefault(_path);
-
-var _atom = require('atom');
-
-var _helpers = require('./helpers');
-
-var _escapeHtml = require('escape-html');
-
-var _escapeHtml2 = _interopRequireDefault(_escapeHtml);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-exports.default = {
+export default {
   config: {
     lintHtmlFiles: {
       title: 'Lint HTML Files',
@@ -67,40 +53,40 @@ exports.default = {
       default: false
     }
   },
-  activate: function () {
-    require('atom-package-deps').install();
+  activate: function() {
+    require('atom-package-deps').install()
 
-    this.subscriptions = new _atom.CompositeDisposable();
-    this.active = true;
-    this.worker = null;
-    this.scopes = ['source.js', 'source.jsx', 'source.js.jsx', 'source.babel', 'source.js-semantic'];
+    this.subscriptions = new CompositeDisposable()
+    this.active = true
+    this.worker = null
+    this.scopes = ['source.js', 'source.jsx', 'source.js.jsx', 'source.babel', 'source.js-semantic']
 
-    const embeddedScope = 'source.js.embedded.html';
+    const embeddedScope = 'source.js.embedded.html'
     this.subscriptions.add(atom.config.observe('linter-eslint.lintHtmlFiles', lintHtmlFiles => {
       if (lintHtmlFiles) {
-        this.scopes.push(embeddedScope);
+        this.scopes.push(embeddedScope)
       } else {
         if (this.scopes.indexOf(embeddedScope) !== -1) {
-          this.scopes.splice(this.scopes.indexOf(embeddedScope), 1);
+          this.scopes.splice(this.scopes.indexOf(embeddedScope), 1)
         }
       }
-    }));
+    }))
     this.subscriptions.add(atom.commands.add('atom-text-editor', {
       'linter-eslint:fix-file': () => {
-        const textEditor = atom.workspace.getActiveTextEditor();
-        const filePath = textEditor.getPath();
-        const fileDir = _path2.default.dirname(filePath);
+        const textEditor = atom.workspace.getActiveTextEditor()
+        const filePath = textEditor.getPath()
+        const fileDir = Path.dirname(filePath)
 
         if (!textEditor || textEditor.isModified()) {
           // Abort for invalid or unsaved text editors
-          atom.notifications.addError('Linter-ESLint: Please save before fixing');
-          return;
+          atom.notifications.addError('Linter-ESLint: Please save before fixing')
+          return
         }
 
         if (this.worker === null) {
           // Abort if worker is not yet ready
-          atom.notifications.addError('Linter-ESLint: Not ready, please try again');
-          return;
+          atom.notifications.addError('Linter-ESLint: Not ready, please try again')
+          return
         }
 
         this.worker.request('FIX', {
@@ -109,13 +95,13 @@ exports.default = {
           global: atom.config.get('linter-eslint.useGlobalEslint'),
           nodePath: atom.config.get('linter-eslint.globalNodePath'),
           configFile: atom.config.get('linter-eslint.eslintrcPath')
-        }).then(function (response) {
-          atom.notifications.addSuccess(response);
-        }).catch(function (response) {
-          atom.notifications.addWarning(response);
-        });
+        }).then(function(response) {
+          atom.notifications.addSuccess(response)
+        }).catch(function(response) {
+          atom.notifications.addWarning(response)
+        })
       }
-    }));
+    }))
 
     // Reason: I (steelbrain) have observed that if we spawn a
     // process while atom is starting up, it can increase startup
@@ -123,44 +109,39 @@ exports.default = {
     // we barely feel a thing.
     const initializeWorker = () => {
       if (this.active) {
-        var _spawnWorker = (0, _helpers.spawnWorker)();
-
-        const child = _spawnWorker.child;
-        const worker = _spawnWorker.worker;
-        const subscription = _spawnWorker.subscription;
-
-        this.worker = worker;
-        this.subscriptions.add(subscription);
+        const {child, worker, subscription} = spawnWorker()
+        this.worker = worker
+        this.subscriptions.add(subscription)
         child.on('exit-linter', shouldLive => {
-          this.worker = null;
+          this.worker = null
           // Respawn if it crashed. See atom/electron#3446
           if (shouldLive) {
-            initializeWorker();
+            initializeWorker()
           }
-        });
+        })
       }
-    };
-    setTimeout(initializeWorker, 5 * 1000);
+    }
+    setTimeout(initializeWorker, 5 * 1000)
   },
-  deactivate: function () {
-    this.active = false;
-    this.subscriptions.dispose();
+  deactivate: function() {
+    this.active = false
+    this.subscriptions.dispose()
   },
-  provideLinter: function () {
-    const Helpers = require('atom-linter');
+  provideLinter: function() {
+    const Helpers = require('atom-linter')
     return {
       name: 'ESLint',
       grammarScopes: this.scopes,
       scope: 'file',
       lintOnFly: true,
       lint: textEditor => {
-        const text = textEditor.getText();
+        const text = textEditor.getText()
         if (text.length === 0) {
-          return Promise.resolve([]);
+          return Promise.resolve([])
         }
-        const filePath = textEditor.getPath();
-        const fileDir = _path2.default.dirname(filePath);
-        const showRule = atom.config.get('linter-eslint.showRuleIdInMessage');
+        const filePath = textEditor.getPath()
+        const fileDir = Path.dirname(filePath)
+        const showRule = atom.config.get('linter-eslint.showRuleIdInMessage')
 
         if (this.worker === null) {
           return Promise.resolve([{
@@ -168,7 +149,7 @@ exports.default = {
             type: 'Info',
             text: 'Worker initialization is delayed. Please try saving or typing to begin linting.',
             range: Helpers.rangeFromLineNumber(textEditor, 0)
-          }]);
+          }])
         }
 
         return this.worker.request('JOB', {
@@ -181,38 +162,32 @@ exports.default = {
           rulesDir: atom.config.get('linter-eslint.eslintRulesDir'),
           configFile: atom.config.get('linter-eslint.eslintrcPath'),
           disableIgnores: atom.config.get('linter-eslint.disableEslintIgnore')
-        }).then(function (response) {
+        }).then(function(response) {
           if (response.length === 1 && response[0].message === 'File ignored because of your .eslintignore file. Use --no-ignore to override.') {
-            return [];
+            return []
           }
-          return response.map(function (_ref) {
-            let message = _ref.message;
-            let line = _ref.line;
-            let severity = _ref.severity;
-            let ruleId = _ref.ruleId;
-            let column = _ref.column;
-
-            const range = Helpers.rangeFromLineNumber(textEditor, line - 1);
+          return response.map(function({message, line, severity, ruleId, column}) {
+            const range = Helpers.rangeFromLineNumber(textEditor, line - 1)
             if (column) {
-              range[0][1] = column - 1;
+              range[0][1] = column - 1
             }
             if (column > range[1][1]) {
-              range[1][1] = column - 1;
+              range[1][1] = column - 1
             }
             const ret = {
               filePath: filePath,
               type: severity === 1 ? 'Warning' : 'Error',
               range: range
-            };
-            if (showRule) {
-              ret.html = `<span class="badge badge-flexible">${ ruleId || 'Fatal' }</span> ${ (0, _escapeHtml2.default)(message) }`;
-            } else {
-              ret.text = message;
             }
-            return ret;
-          });
-        });
+            if (showRule) {
+              ret.html = `<span class="badge badge-flexible">${ruleId || 'Fatal'}</span> ${escapeHTML(message)}`
+            } else {
+              ret.text = message
+            }
+            return ret
+          })
+        })
       }
-    };
+    }
   }
-};
+}
