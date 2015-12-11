69c9fe76bd8fdefb89aa21bfe957db1ec05b3b22
steelbrain <aneesiqbalbhatti@gmail.com>
2015-12-11 01:17:42 -0700

    :new: Merge advance Helpers from improve branch

--- src/helpers.js
+++ src/helpers.js
@@ -1,9 +1,9 @@
 'use babel'
 
+import Path from 'path'
+import FS from 'fs'
 import ChildProcess from 'child_process'
 import CP from 'childprocess-promise'
-import FS from 'fs'
-import Path from 'path'
 import {find} from 'atom-linter'
 
 export function spawnWorker() {
@@ -38,65 +38,17 @@ export function spawnWorker() {
   }}}
 }
 
-let prefixPath = null
-const atomEslintPath = Path.normalize(Path.join(__dirname, '..', 'node_modules', 'eslint'))
-
-export function findEslintDir(params) {
-  const modulesPath = find(params.fileDir, 'node_modules')
-  let eslintNewPath = null
-
-  if (params.global) {
-    if (params.nodePath === '' && prefixPath === null) {
-      const npmCommand = process.platform === 'win32' ? 'npm.cmd' : 'npm'
-      try {
-        prefixPath = ChildProcess.spawnSync(npmCommand, ['get', 'prefix']).output[1].toString().trim()
-      } catch (e) {
-        throw new Error('Unable to execute `npm get prefix`. Please make sure Atom is getting $PATH correctly')
-      }
-    }
-    if (process.platform === 'win32') {
-      eslintNewPath = Path.join(params.nodePath || prefixPath, 'node_modules', 'eslint')
-    } else {
-      eslintNewPath = Path.join(params.nodePath || prefixPath, 'lib', 'node_modules', 'eslint')
-    }
-  } else {
-    try {
-      FS.accessSync(eslintNewPath = Path.join(modulesPath, 'eslint'), FS.R_OK)
-    } catch (_) {
-      eslintNewPath = atomEslintPath
-    }
-  }
-
-  return eslintNewPath
+export function getModulesDirectory(fileDir) {
+  return find(fileDir, 'node_modules')
 }
 
-// Check for project config file or eslint config in package.json and determine
-// whether to bail out or use config specified in package options
-export function determineConfigFile(params) {
-  // config file
-  const configFile = find(params.fileDir, ['.eslintrc.js', '.eslintrc.yaml', '.eslintrc.yml', '.eslintrc.json', '.eslintrc']) || null
-  if (configFile) {
-    return configFile
-  }
-  // package.json
-  const packagePath = find(params.fileDir, 'package.json')
-  if (packagePath && Boolean(require(packagePath).eslintConfig)) {
-    return packagePath
-  }
-  // Couldn't find a config
-  if (params.canDisable) {
-    return null
-  }
-  // If all else fails, use the configFile specified in the linter-eslint options
-  if (params.configFile) {
-    return params.configFile
-  }
+export function getIgnoresFile(fileDir) {
+  return Path.dirname(find(fileDir, '.eslintignore'))
 }
 
-export function getEslintCli(path) {
+export function getEslintFromDirectory(path) {
   try {
-    const eslint = require(Path.join(path, 'lib', 'cli.js'))
-    return eslint
+    return require(Path.join(path, 'lib', 'cli.js'))
   } catch (e) {
     if (e.code === 'MODULE_NOT_FOUND') {
       throw new Error('ESLint not found, Please install or make sure Atom is getting $PATH correctly')
@@ -104,4 +56,83 @@ export function getEslintCli(path) {
   }
 }
 
-export {find}
+let nodePrefixPath = null
+
+export function getNodePrefixPath() {
+  if (nodePrefixPath === null) {
+    const npmCommand = process.platform === 'win32' ? 'npm.cmd' : 'npm'
+    try {
+      nodePrefixPath = ChildProcess.spawnSync(npmCommand, ['get', 'prefix']).output[1].toString().trim()
+    } catch (e) {
+      throw new Error('Unable to execute `npm get prefix`. Please make sure Atom is getting $PATH correctly')
+    }
+  }
+  return nodePrefixPath
+}
+
+let bundledEslintDirectory = null
+
+export function getBundledEslintDirectory() {
+  if (bundledEslintDirectory === null) {
+    bundledEslintDirectory = Path.normalize(Path.join(__dirname, '..', 'node_modules', 'eslint'))
+  }
+  return bundledEslintDirectory
+}
+
+export function getEslintDirectory(params, modulesPath = null) {
+  if (params.global) {
+    const prefixPath = getNodePrefixPath()
+    if (process.platform === 'win32') {
+      return Path.join(params.nodePath || prefixPath, 'node_modules', 'eslint')
+    }
+    return Path.join(params.nodePath || prefixPath, 'lib', 'node_modules', 'eslint')
+  }
+  const eslintPath = Path.join(modulesPath || getModulesDirectory(params.fileDir), 'eslint')
+  try {
+    FS.accessSync(eslintPath, FS.R_OK)
+    return eslintPath
+  } catch (_) {
+    return getBundledEslintDirectory()
+  }
+}
+
+export function getEslintConfig(params) {
+  const configFile = find(params.fileDir, ['.eslintrc.js', '.eslintrc.yaml', '.eslintrc.yml', '.eslintrc.json', '.eslintrc']) || null
+  if (configFile) {
+    return configFile
+  }
+
+  const packagePath = find(params.fileDir, 'package.json')
+  if (packagePath && Boolean(require(packagePath).eslintConfig)) {
+    return packagePath
+  }
+
+  if (params.canDisable) {
+    return null
+  }
+
+  if (params.configFile) {
+    return params.configFile
+  }
+}
+
+let eslint
+let lastEslintDirectory
+let lastModulesPath
+
+export function getEslint(params) {
+  const modulesPath = getModulesDirectory(params.fileDir)
+  const eslintDirectory = getEslintDirectory(params, modulesPath)
+  if (eslintDirectory !== lastEslintDirectory) {
+    lastEslintDirectory = eslintDirectory
+    eslint = getEslintFromDirectory(eslintDirectory)
+  }
+  if (lastModulesPath !== modulesPath) {
+    lastModulesPath = modulesPath
+    if (modulesPath) {
+      process.env.NODE_PATH = modulesPath
+    } else process.env.NODE_PATH = ''
+    require('module').Module._initPaths()
+  }
+  return {eslint, eslintDirectory}
+}
