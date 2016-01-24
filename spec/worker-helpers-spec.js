12c35db52617fed49a5cd628cc3dfa6f12a0d9c8
steelbrain <aneesiqbalbhatti@gmail.com>
2016-01-24 06:32:54 -0700

    :new: Add specs for getRelativePath

--- spec/worker-helpers-spec.js
+++ spec/worker-helpers-spec.js
@@ -2,7 +2,7 @@
 
 import * as Helpers from '../lib/worker-helpers'
 import {getFixturesPath} from './common'
-import {join as joinPath, normalize as normalizePath} from 'path'
+import * as Path from 'path'
 
 describe('Worker Helpers', function() {
 
@@ -36,29 +36,44 @@ describe('Worker Helpers', function() {
   describe('getConfigPath', function() {
     it('finds .eslintrc', function() {
       const fileDir = getFixturesPath('configs/no-ext/')
-      const expectedPath = joinPath(fileDir, '.eslintrc')
+      const expectedPath = Path.join(fileDir, '.eslintrc')
       expect(Helpers.getConfigPath(fileDir)).toBe(expectedPath)
     })
     it('finds .eslintrc.yaml', function() {
       const fileDir = getFixturesPath('configs/yaml/')
-      const expectedPath = joinPath(fileDir, '.eslintrc.yaml')
+      const expectedPath = Path.join(fileDir, '.eslintrc.yaml')
       expect(Helpers.getConfigPath(fileDir)).toBe(expectedPath)
     })
     it('finds .eslintrc.yml', function() {
       const fileDir = getFixturesPath('configs/yml/')
-      const expectedPath = joinPath(fileDir, '.eslintrc.yml')
+      const expectedPath = Path.join(fileDir, '.eslintrc.yml')
       expect(Helpers.getConfigPath(fileDir)).toBe(expectedPath)
     })
     it('finds .eslintrc.js', function() {
       const fileDir = getFixturesPath('configs/js/')
-      const expectedPath = joinPath(fileDir, '.eslintrc.js')
+      const expectedPath = Path.join(fileDir, '.eslintrc.js')
       expect(Helpers.getConfigPath(fileDir)).toBe(expectedPath)
     })
     it('finds .eslintrc.json', function() {
       const fileDir = getFixturesPath('configs/json/')
-      const expectedPath = joinPath(fileDir, '.eslintrc.json')
+      const expectedPath = Path.join(fileDir, '.eslintrc.json')
       expect(Helpers.getConfigPath(fileDir)).toBe(expectedPath)
     })
   })
 
+  describe('getRelativePath', function() {
+    it('return path relative of ignore file if found', function() {
+      const fixtureDir = getFixturesPath('eslintignore')
+      const fixtureFile = Path.join(fixtureDir, 'ignored.js')
+      const relativePath = Helpers.getRelativePath(fixtureDir, fixtureFile, {})
+      const expectedPath = Path.relative(__dirname + '/../', fixtureFile)
+      expect(relativePath).toBe(expectedPath)
+    })
+    it('does not return path relative to ignore file if config overrides it', function() {
+      const fixtureDir = getFixturesPath('eslintignore')
+      const fixtureFile = Path.join(fixtureDir, 'ignored.js')
+      const relativePath = Helpers.getRelativePath(fixtureDir, fixtureFile, {disableEslintIgnore: true})
+      expect(relativePath).toBe('ignored.js')
+    })
+  })
 })
