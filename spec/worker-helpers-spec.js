f5768ac7f3d1279058d33652d75d3e1d041213fe
Landon Abney <Landonabney@gmail.com>
2016-01-25 11:16:14 -0800

    Fix lint errors

--- spec/worker-helpers-spec.js
+++ spec/worker-helpers-spec.js
@@ -4,27 +4,27 @@ import * as Helpers from '../lib/worker-helpers'
 import { getFixturesPath } from './common'
 import * as Path from 'path'
 
-describe('Worker Helpers', function () {
-  describe('getESLintInstance && getESLintFromDirectory', function () {
-    it('tries to find a local eslint', function () {
+describe('Worker Helpers', () => {
+  describe('getESLintInstance && getESLintFromDirectory', () => {
+    it('tries to find a local eslint', () => {
       const eslint = Helpers.getESLintInstance(getFixturesPath('local-eslint'), {})
       expect(eslint).toBe('located')
     })
-    it('cries if local eslint is not found', function () {
-      expect(function () {
+    it('cries if local eslint is not found', () => {
+      expect(() => {
         Helpers.getESLintInstance(getFixturesPath('files', {}))
       }).toThrow()
     })
 
-    it('tries to find a global eslint if config is specified', function () {
+    it('tries to find a global eslint if config is specified', () => {
       const eslint = Helpers.getESLintInstance(getFixturesPath('local-eslint'), {
         useGlobalEslint: true,
         globalNodePath: getFixturesPath('global-eslint')
       })
       expect(eslint).toBe('located')
     })
-    it('cries if global eslint is not found', function () {
-      expect(function () {
+    it('cries if global eslint is not found', () => {
+      expect(() => {
         Helpers.getESLintInstance(getFixturesPath('local-eslint'), {
           useGlobalEslint: true
         })
@@ -32,43 +32,43 @@ describe('Worker Helpers', function () {
     })
   })
 
-  describe('getConfigPath', function () {
-    it('finds .eslintrc', function () {
-      const fileDir = getFixturesPath('configs/no-ext/')
+  describe('getConfigPath', () => {
+    it('finds .eslintrc', () => {
+      const fileDir = getFixturesPath(Path.join('configs', 'no-ext'))
       const expectedPath = Path.join(fileDir, '.eslintrc')
       expect(Helpers.getConfigPath(fileDir)).toBe(expectedPath)
     })
-    it('finds .eslintrc.yaml', function () {
-      const fileDir = getFixturesPath('configs/yaml/')
+    it('finds .eslintrc.yaml', () => {
+      const fileDir = getFixturesPath(Path.join('configs', 'yaml'))
       const expectedPath = Path.join(fileDir, '.eslintrc.yaml')
       expect(Helpers.getConfigPath(fileDir)).toBe(expectedPath)
     })
-    it('finds .eslintrc.yml', function () {
-      const fileDir = getFixturesPath('configs/yml/')
+    it('finds .eslintrc.yml', () => {
+      const fileDir = getFixturesPath(Path.join('configs', 'yml'))
       const expectedPath = Path.join(fileDir, '.eslintrc.yml')
       expect(Helpers.getConfigPath(fileDir)).toBe(expectedPath)
     })
-    it('finds .eslintrc.js', function () {
-      const fileDir = getFixturesPath('configs/js/')
+    it('finds .eslintrc.js', () => {
+      const fileDir = getFixturesPath(Path.join('configs', 'js'))
       const expectedPath = Path.join(fileDir, '.eslintrc.js')
       expect(Helpers.getConfigPath(fileDir)).toBe(expectedPath)
     })
-    it('finds .eslintrc.json', function () {
-      const fileDir = getFixturesPath('configs/json/')
+    it('finds .eslintrc.json', () => {
+      const fileDir = getFixturesPath(Path.join('configs', 'json'))
       const expectedPath = Path.join(fileDir, '.eslintrc.json')
       expect(Helpers.getConfigPath(fileDir)).toBe(expectedPath)
     })
   })
 
-  describe('getRelativePath', function () {
-    it('return path relative of ignore file if found', function () {
+  describe('getRelativePath', () => {
+    it('return path relative of ignore file if found', () => {
       const fixtureDir = getFixturesPath('eslintignore')
       const fixtureFile = Path.join(fixtureDir, 'ignored.js')
       const relativePath = Helpers.getRelativePath(fixtureDir, fixtureFile, {})
-      const expectedPath = Path.relative(__dirname + '/../', fixtureFile)
+      const expectedPath = Path.relative(Path.join(__dirname, '..'), fixtureFile)
       expect(relativePath).toBe(expectedPath)
     })
-    it('does not return path relative to ignore file if config overrides it', function () {
+    it('does not return path relative to ignore file if config overrides it', () => {
       const fixtureDir = getFixturesPath('eslintignore')
       const fixtureFile = Path.join(fixtureDir, 'ignored.js')
       const relativePath =
