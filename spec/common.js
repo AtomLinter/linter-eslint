35aa9f4f1f8c6a631964a68337f5fdd5fd172c0b
Landon Abney <Landonabney@gmail.com>
2016-04-19 23:46:35 -0700

    Remove 'use strict'
    This isn't necessary inside a module, which these files are.

--- spec/common.js
+++ spec/common.js
@@ -1,5 +1,3 @@
-'use strict'
-
 const Path = require('path')
 
 module.exports.getFixturesPath = function (path) {
--- src/reporter.js
+++ src/reporter.js
@@ -1,4 +1,3 @@
-'use strict'
 // This file is used by eslint to hand the errors over to the worker
 module.exports = function (results) {
   global.__LINTER_ESLINT_RESPONSE = results[0].messages
