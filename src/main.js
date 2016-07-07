ef8451dbb40deec85f8919d115b7ba9f95a8678e
Eric Bower <neurosnap@gmail.com>
2016-07-07 19:42:10 -0400

    Make scopes a configuration setting (#629)
    * adding scopes to config
    
    * observe scopes config and update when it changes

--- src/main.js
+++ src/main.js
@@ -11,8 +11,14 @@ module.exports = {
     this.subscriptions = new CompositeDisposable()
     this.active = true
     this.worker = null
-    this.scopes = ['source.js', 'source.jsx', 'source.js.jsx', 'source.babel', 'source.js-semantic']
+    this.scopes = []
 
+    this.subscriptions.add(atom.config.observe('linter-eslint.scopes', scopes => {
+      // Remove any old scopes
+      this.scopes.splice(0, this.scopes.length)
+      // Add the current scopes
+      Array.prototype.push.apply(this.scopes, scopes)
+    }))
     const embeddedScope = 'source.js.embedded.html'
     this.subscriptions.add(atom.config.observe('linter-eslint.lintHtmlFiles', lintHtmlFiles => {
       if (lintHtmlFiles) {
