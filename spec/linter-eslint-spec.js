9a685cf629dc5bfbb3bf8942b970a0692e3f0fef
Ian VanSchooten <ian.vanschooten@gmail.com>
2016-09-25 00:51:30 -0400

    Fix specs for ‘fix’ functionality
    The spec was previously attempting to test the fix-on-save functionality,
    but unfortunately due to the way `lint` is set up in the specs, the 
    logic was never exercised which would have applied fixing after a save.
    
    So, the file was being linted, saved without changing, and then there
    were some errors thrown which prevented all of the assertions from being
    hit.
    
    This change simplifies the test to only exercise the `fix` type of worker
    job.  It does this by:
    
    1. Copying the fixture file and a config file to a temporary directory
    2. Linting the file to ensure it has a few errors
    3. Setting a watcher on the file which will detect when it changes and
    assert that there are no longer errors
    4. Performing a fix on the file, which should change the file and kick off
    the watcher.
    5. Removing the copied files and temp directory

--- spec/linter-eslint-spec.js
+++ spec/linter-eslint-spec.js
@@ -1,12 +1,15 @@
 'use babel'
 
 import * as path from 'path'
+import * as fs from 'fs'
+import { tmpdir } from 'os'
 import linter from '../lib/main'
 
 const goodPath = path.join(__dirname, 'fixtures', 'files', 'good.js')
 const badPath = path.join(__dirname, 'fixtures', 'files', 'bad.js')
 const emptyPath = path.join(__dirname, 'fixtures', 'files', 'empty.js')
 const fixPath = path.join(__dirname, 'fixtures', 'files', 'fix.js')
+const configPath = path.join(__dirname, 'fixtures', 'configs', '.eslintrc.yml')
 const importingpath = path.join(__dirname, 'fixtures',
   'import-resolution', 'nested', 'importing.js')
 const badImportPath = path.join(__dirname, 'fixtures',
@@ -19,6 +22,13 @@ describe('The eslint provider for Linter', () => {
 
   const worker = spawnWorker()
   const lint = linter.provideLinter.call(worker).lint
+  const fix = textEditor =>
+    worker.worker.request('job', {
+      contents: textEditor.getText(),
+      type: 'fix',
+      config: atom.config.get('linter-eslint'),
+      filePath: textEditor.getPath()
+    })
 
   beforeEach(() => {
     atom.config.set('linter-eslint.disableFSCache', false)
@@ -128,21 +138,72 @@ describe('The eslint provider for Linter', () => {
     })
   })
 
-  describe('Fix errors when saved', () => {
+  describe('Fix errors', () => {
+    let editor
+    let doneCheckingFixes
+    let tempFixtureDir
+    let tempFixturePath
+    let tempConfigPath
+
     beforeEach(() => {
-      atom.config.set('linter-eslint.fixOnSave', true)
-    })
-    it('should fix lint errors when saved', () => {
-      waitsForPromise(() =>
-        atom.workspace.open(fixPath).then((editor) => {
-          lint(editor).then((messages) => {
-            expect(messages.length).toBe(2)
-            editor.save()
-            lint(editor).then((messagesAfterSave) => {
-              expect(messagesAfterSave.length).toBe(0)
-            })
-          })
+      waitsForPromise(() => {
+        tempFixtureDir = fs.mkdtempSync(tmpdir() + path.sep)
+        tempFixturePath = `${tempFixtureDir}${path.sep}fixed.js`
+        tempConfigPath = `${tempFixtureDir}${path.sep}.eslintrc.yaml`
+        fs.createReadStream(configPath).pipe(fs.createWriteStream(tempConfigPath))
+
+        return atom.workspace.open(fixPath).then((openEditor) => {
+          openEditor.saveAs(tempFixturePath)
+          editor = openEditor
         })
+      })
+    })
+
+    afterEach(() => {
+      fs.unlinkSync(tempFixturePath)
+      fs.unlinkSync(tempConfigPath)
+      fs.rmdirSync(tempFixtureDir)
+    })
+
+    it('should fix linting errors', () => {
+      function firstLint(textEditor) {
+        return lint(textEditor)
+        .then((messages) => {
+          // The original file has two errors
+          expect(messages.length).toBe(2)
+          return textEditor
+        })
+      }
+      function makeFixes(textEditor) {
+        return fix(textEditor)
+          .then((messagesAfterSave) => {
+            // Linter reports a successful fix
+            expect(messagesAfterSave).toBe('Linter-ESLint: Fix Complete')
+            return textEditor
+          })
+      }
+      // Create a subscription to watch when the editor changes (from the fix)
+      editor.onDidChange(() => {
+        lint(editor)
+          .then((messagesAfterFixing) => {
+            // Note: this fires several times, with only the final time resulting in
+            // a non-null messagesAfterFixing.  This is the reason for the check here
+            // and for the `waitsFor` which makes sure the expectation is tested.
+            if (messagesAfterFixing) {
+              // After opening the file again, there are no linting errors
+              expect(messagesAfterFixing.length).toBe(0)
+              doneCheckingFixes = true
+            }
+          })
+      })
+
+      waitsForPromise(() =>
+        firstLint(editor)
+          .then(makeFixes)
+      )
+      waitsFor(
+        () => doneCheckingFixes,
+        'Messages should be checked after fixing'
       )
     })
   })
