aa3e5583db8918514cb55b6b71c78b61ebf33f2a
Ian VanSchooten <ian.vanschooten@gmail.com>
2017-09-27 08:52:31 -0400

    Add options to jsdoc

--- src/helpers.js
+++ src/helpers.js
@@ -143,6 +143,9 @@ export async function generateDebugString(worker) {
  * Turn the given options into a Linter message array
  * @param  {TextEditor} textEditor The TextEditor to use to build the message
  * @param  {Object} options    The parameters used to fill in the message
+ * @param  {string} [options.severity='error'] Can be one of: 'error', 'warning', 'info'
+ * @param  {string} [options.excerpt=''] Short text to use in the message
+ * @param  {string|Function} [options.description] Used to provide additional information
  * @return {Array}            Message to user generated from the parameters
  */
 export function generateUserMessage(textEditor, options) {
